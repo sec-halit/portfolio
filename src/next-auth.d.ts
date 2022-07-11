29e59a2382bc666095e241f38061df992081eabd
sec-halit <102877470+sec-halit@users.noreply.github.com>
2022-07-11 18:40:23 +0300

    dev-01 fixed ts

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,11 +1,32 @@
-import "next-auth/jwt"
+// import "next-auth/jwt";
+// import NextAuth from "next-auth";
+// // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 
-// Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
+// declare module "next-auth/jwt" {
+//   interface JWT {
+//     idToken?: string;
+//     accessToken?:string;
+//     /** The user's role. */
+//     userRole?: "admin";
+//     accessToken?: string;
+//   }
+// }
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    /** The user's role. */
-    userRole?: "admin",
-    accessToken?:string
-  }
-}
+// declare module "next-auth" {
+//   /**
+//    * Returned by `useSession`, `getSession` and received as a prop on the `Provider` React Context
+//    */
+//   interface Session {
+//     user: {
+//       address: string;
+//     };
+//   }
+//   interface User {}
+//   /**
+//    * Usually contains information about the provider being used
+//    * and also extends `TokenSet`, which is different tokens returned by OAuth Providers.
+//    */
+//   interface Account {}
+//   /** The OAuth profile returned from your provider */
+//   interface Profile {}
+// }
--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,4 +1,4 @@
-import * as NextAuth  from 'next-auth';
+import NextAuth from 'next-auth';
 
 
 import GoogleProvider from "next-auth/providers/google";
