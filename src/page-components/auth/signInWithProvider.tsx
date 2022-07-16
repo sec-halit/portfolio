e20d7f0a163055bce5f892589e456ab4a36876f5
sec-halit <102877470+sec-halit@users.noreply.github.com>
2022-07-16 13:51:01 +0300

    dev-06 fix type

--- src/page-components/auth/signInWithProvider.tsx
+++ src/page-components/auth/signInWithProvider.tsx
@@ -27,7 +27,7 @@ const SignInWithProvider: FC<Props> = ({
 }): ReactElement => {
     const lazyRoot = React.useRef(null)
     const { signinUrl, name, type,callbackUrl } = provider || {}
-    const onSubmit = async (e)=>{
+    const onSubmit = async (e:EventTarget)=>{
         e.preventDefault();
         await signIn(provider?.id,{
                signinUrl:"/auth/login/"+(provider?.id || ""),
