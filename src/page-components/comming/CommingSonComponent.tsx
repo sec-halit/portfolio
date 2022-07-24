8e850e81ddcb29543865be9aaa216329180d499a
sec-halit <102877470+sec-halit@users.noreply.github.com>
2022-07-24 19:46:25 +0300

    dev-00001 fix bg color

--- src/page-components/comming/CommingSonComponent.tsx
+++ src/page-components/comming/CommingSonComponent.tsx
@@ -4,6 +4,9 @@ interface IComingSoonProps {
 
 const ComingSoon: FC<IComingSoonProps> = () => {
   return <>
+    <div className="layout-image-content">
+      
+    </div>
     <div className="layout-image" style={{backgroundImage:"url('"+(new URL("@/images/coming-soon/coming-soon-pages.svg",import.meta.url).pathname)+"')"}}>
         <div className="typewriter">
             <h1 className="custom-shadow">Coming Soon</h1>
