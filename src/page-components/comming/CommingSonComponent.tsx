6c95ad2dc5036103218965d12376e68e967a1dda
sec-halit <102877470+sec-halit@users.noreply.github.com>
2022-07-24 15:37:19 +0300

    dev-00001 custom shadow

--- src/page-components/comming/CommingSonComponent.tsx
+++ src/page-components/comming/CommingSonComponent.tsx
@@ -6,7 +6,7 @@ const ComingSoon: FC<IComingSoonProps> = () => {
   return <>
     <div className="layout-image" style={{backgroundImage:"url('"+(new URL("@/images/coming-soon/coming-soon-pages.svg",import.meta.url).pathname)+"')"}}>
         <div className="typewriter">
-            <h1>Coming Soon</h1>
+            <h1 className="custom-shadow">Coming Soon</h1>
         </div>
     </div>
   </>;
